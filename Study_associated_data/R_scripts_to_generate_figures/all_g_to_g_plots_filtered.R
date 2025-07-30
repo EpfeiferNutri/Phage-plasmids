acb4030932beecf8225c6a0fccf9daec893f6d98
EpfeiferNutri <162160809+EpfeiferNutri@users.noreply.github.com>
2025-07-30 17:49:08 +0200

    Add files via upload

---
+++ Study_associated_data/R_scripts_to_generate_figures/all_g_to_g_plots_filtered.R
@@ -0,0 +1,121 @@
+
+########## Script for generating genome-to-genome comparative plots for selected genomes
+
+# required libraries
+
+library(data.table) 
+library(gggenomes)  
+library(ggplot2)   
+library(dplyr)     
+library(ggnewscale)
+library(tidyverse) 
+
+############ Reading the data tables required for generating the figures
+
+# Genome sequences used for plotting 
+genomes = fread("Study_associated_data/Supplementary_data/g2g_plot_tables/genomes_used_gggenomes.tsv")
+
+# Protein information necessary for gggenomes plots (coordinates, protein IDs, etc.)
+protein_info = fread("Study_associated_data/Supplementary_data/g2g_plot_tables/protein_info_used_gggenomes.tsv")
+
+# Best Bi-directional Hit (BBH) protein-protein links
+# Note: seq_id == query, seq_id2 == subject
+BBH_links = fread("Study_associated_data/Supplementary_data/g2g_plot_tables/BBH_links_used_gggenomes.tsv")
+
+# Table with wGRR (weighted Gene Repertoire Relatedness) values for selected genomes 
+wGRR_info = fread("/Study_associated_data/Supplementary_data/g2g_plot_tables/wGRR_info_used_gggenomes.tsv")
+
+############ Defining the genome-to-genome comparative plotting function
+
+plot_gggenomes = function(genomes_list_, protein_info_, links_) {
+  
+  gggenomes(seqs = genomes_list_, genes = protein_info_, links = links_) +
+    geom_seq() +        
+    geom_gene(aes(fill = is_persistent)) +   # color genes by persistence status
+    scale_fill_manual(values = c("limegreen", "grey80")) +
+    
+    new_scale_fill() +  
+    
+    geom_link(aes(fill = pident), color = alpha("white", alpha = 0), offset = 0.08) +
+    scale_fill_gradient2(
+      limits = c(0.35, 1), 
+      breaks = c(0.35, 0.5, 0.65, 0.8, 0.95),
+      low = alpha("white", alpha = 0.0),
+      midpoint = 0.5, 
+      mid = alpha("tomato", alpha = 1), 
+      high = alpha("darkgreen", alpha = 1)) +
+    
+    guides(colour = guide_colorbar(), direction = "horizontal") +   # colorbar for homology identity
+    geom_bin_label(size = 8, expand_left = 0.2) +
+    
+    theme(
+      axis.text.x = element_text(size = 20),
+      legend.title = element_blank(),
+      # legend.box = "horizontal",
+      legend.position = "right") +
+    
+    scale_x_bp(suffix = "b", sep = " ")
+  
+}
+
+############# Defining the processing function: prepare data for a specific Phage-Plasmid group
+
+process_pp_type = function(PP_type, genome_list_i) {
+  
+  # Subset genomes and order according to user-defined list
+  genomes_i = genomes %>% filter(seq_id %in% genome_list_i)%>%
+    mutate(seq_id = factor(seq_id, levels = genome_list_i)) %>% arrange(seq_id)
+  
+  # Subset BBH links and protein information for selected genomes
+  links_i = BBH_links %>% filter(seq_id %in% genomes_i$seq_id)
+  protein_info_i = protein_info %>% filter(seq_id %in% genomes_i$seq_id)
+  
+  # Generate gggenomes plot
+  plot_gggenomes(genomes_list_ = genomes_i, protein_info_ = protein_info_i, links_ = links_i)
+
+}
+
+############ Generating the plots for each figure and dataset
+
+################# Examples of hits (low confidence) with atypical sizes: 
+# --------------- pMT1 long genomes ----------------
+genome_list_pMT1 = c("NZ_CP045146", "NZ_CP010248")
+process_pp_type(PP_type = "pMT1", genome_list_i = genome_list_pMT1)
+# --------------- pKpn validation ----------------
+genome_list_pKpn = c("NZ_CP017778",'NZ_CP028550', 'NZ_CP027149')
+process_pp_type(PP_type = "pKpn", genome_list_i = genome_list_pKpn)
+
+################# cp32 elements with high, medium and low confidence levels:
+genome_list_cp32 = c("NZ_CP115597", "NZ_CP114697", "NZ_CP073149", "NZ_CP019758", "NZ_CP019926")
+process_pp_type(PP_type = "cp32", genome_list_i = genome_list_cp32)
+
+################# Detection of P1-like plasmids (false-positives) by tyPPing:
+genome_list_P1_1_1 = c('NC_005856', 'NZ_CP010146', "NZ_LR883966", "NZ_CP061756")
+process_pp_type(PP_type = "P1_1", genome_list_i = genome_list_P1_1_1)
+
+################# N15-like P-Ps with atypical sizes:
+genome_list_N15 = c("NZ_CP064249", "NZ_CP070567", "NZ_CP082016")
+process_pp_type(PP_type = "N15", genome_list_i = genome_list_N15)
+
+################# P1_1 low confidence with too-long size:
+genome_list_P1_1_2 = c("NZ_CP081715",'NZ_CP103719', 'NC_005856')
+process_pp_type(PP_type = "P1_1", genome_list_i = genome_list_P1_1_2)
+
+################# P1_2 case (false positive) predicted by tyPPing in 05/23:
+genome_list_P1_2 = c("NZ_CP100729", "NZ_CP032492")
+process_pp_type(PP_type = "P1_2", genome_list_i = genome_list_P1_2)
+
+################# Cases (low confidence) related to AB_1, detected by MM-GRC and not by tyPPing:
+genome_list_AB = c( 'NZ_CP107600',                            # virus, +tyPPing
+                    "NZ_CP069498",                            # provirus, -tyPPing
+                    "NZ_CP102935", "NZ_CP102763")             # provirus, -tyPPing, -vcontact
+process_pp_type(PP_type = "AB", genome_list_i = genome_list_AB)
+
+################# SSU5_pHCM2-like P-P not detected by tyPPing :
+genome_list_SSU5 = c("NZ_CP110081", "NZ_CP077310")
+process_pp_type(PP_type = "SSU5_pHCM2", genome_list_i = genome_list_SSU5)
+
+################# pSLy3-like P-P not detected by tyPPing:
+genome_list_pSLy3 = c("NZ_CP031294", "NZ_CP074043")
+process_pp_type(PP_type = "pSLy3", genome_list_i = genome_list_pSLy3)
+
---
+++ Study_associated_data/R_scripts_to_generate_figures/figures_section_1.R
@@ -0,0 +1,194 @@
+# libraries
+
+library(data.table)
+library(scales)
+library(readxl)
+library(tidyverse)
+
+
+############### Figure 1A: Profile generation 
+
+# number of genomes used to generate signature profiles
+N_genomes =  read_excel( "Study_associated_data/Supplementary_data/S1_All_MGE_information_table.xlsx") %>% 
+  filter(`MGE type MM-GRC` %in% c("N15 group", "AB_1 subgroup", "cp32 group", 
+                                      "P1_1 subgroup", "P1_2 subgroup", 
+                                      "pKpn group", "pSLy3 group", "pCAV group", "pMT1 group", "SSU5_pHCM2 group") & `in 03/21` == "Yes") %>%
+  mutate(`P-P type` = str_remove(`MGE type MM-GRC`, " (group|subgroup)$")) %>%
+  select(`NCBI accession`, Source, `P-P type`) %>% 
+  group_by(Source, `P-P type`) %>% summarise(count = n())
+
+ggplot(N_genomes, aes(fill=Source, y=count, x=`P-P type`)) + 
+  geom_bar(position="stack", stat="identity", width = 0.6) +
+  coord_flip() + scale_y_reverse() + 
+  theme_bw() + 
+  ylab("Number of genomes") + xlab("P-P type")+
+  theme(strip.background =element_blank(), text = element_text(size = 20)) +
+  scale_fill_manual(values = c("#00AAFF", "#FFA64D"))
+
+# number of generated signature profiles
+N_signature_genes = read_excel("Study_associated_data/Supplementary_data/T1_Cutoffs_information_table.xlsx")%>% 
+  select(`P-P type`, `Total number of signature genes`) 
+
+ggplot(N_signature_genes, aes(y = `Total number of signature genes`, x = `P-P type`)) + 
+  geom_bar(position = "stack", stat = "identity", fill = "#38A856", width = 0.6) + 
+  scale_y_continuous(breaks = seq(0, 125, 20)) +
+  coord_flip() + 
+  theme_bw() + 
+  xlab("P-P type") + 
+  ylab("Number of generated HMMs") + 
+  theme(strip.background = element_blank(), text = element_text(size = 20), axis.text.y = element_text(face = "bold"))
+
+
+
+############### Figure 1B: Ratio 'Signature genes / All genes'
+
+Ratio_signature_to_all_0321 =  read_excel( "Study_associated_data/Supplementary_data/S1_All_MGE_information_table.xlsx") %>% 
+  filter(`MGE type MM-GRC` %in% c("N15 group", "AB_1 subgroup", "cp32 group", 
+                                      "P1_1 subgroup", "P1_2 subgroup", 
+                                      "pKpn group", "pSLy3 group", "pCAV group", "pMT1 group", "SSU5_pHCM2 group") & `in 03/21` == "Yes") %>%
+  mutate(`P-P type` = str_remove(`MGE type MM-GRC`, " (group|subgroup)$")) %>%
+  select(`NCBI accession`, Ratio = `Signature to all genes ratio`, `P-P type`)
+
+Ratio_signature_to_all_0321 %>% 
+  ggplot(aes(x = `P-P type`, y = Ratio)) +
+  geom_boxplot(outlier.shape = NA, fill = "#2ea108", alpha = 0.7) +
+  geom_jitter(width = 0.1, color = "darkgreen") +
+  theme_bw() + 
+  scale_y_continuous(breaks = seq(0, 1, 0.1)) +
+  xlab("P-P type") + 
+  ylab("Ratio") +
+  theme(strip.background = element_blank(), text = element_text(size = 18)) 
+
+
+############### Signature_gene_ratio_statistics
+
+Ratio_signature_to_all_0321_stat = Ratio_signature_to_all_0321 %>% 
+  group_by(`P-P type`) %>%
+  summarise(
+  count = n(),
+  mean_Ratio = mean(Ratio, na.rm = TRUE),
+  median_Ratio = median(Ratio, na.rm = TRUE),
+  sd_Ratio = sd(Ratio, na.rm = TRUE),
+  min_Ratio = min(Ratio, na.rm = TRUE),
+  max_Ratio = max(Ratio, na.rm = TRUE))
+
+
+############### Figure 1C: P-P score graphs
+
+Profile_information = read_excel("Study_associated_data/Supplementary_data/S2_Profile_information_table.xlsx")
+
+ggplot(Profile_information, aes(x=x) ) +
+  geom_histogram( aes(x = `P-P score`), bins = 30 , fill="#378B85", col = "#0F595D" ) +
+  # geom_label( aes(x=0.3, y=150, label="P-P scores"), color="#378B85", size = 7) +
+  geom_histogram( aes(x = `P-P type score`,y=after_stat(-..count..)), bins = 30 , fill= "#88BB00", col = "#4C7500") +
+  # geom_label( aes(x=0.3, y=-150, label="P-P type scores"), color="#88BB00", size = 7) +
+  theme_bw() + 
+  scale_y_continuous(breaks = pretty_breaks(n = 10))+
+  scale_x_continuous(breaks = seq(0,1,0.1))+
+  xlab(" Single profile specificity score values") + ylab("Number of profiles")+
+  theme(strip.background =element_blank(), text = element_text(size = 18))
+
+
+############### Figure 1D: Profile annotation with PHROGs (all types together)
+
+Profile_information = read_excel("Study_associated_data/Supplementary_data/S2_Profile_information_table.xlsx") %>% 
+  mutate(`PHROG/rep/par annotation` = case_when(!is.na(`PHROGs Annotation`) ~ `PHROGs Annotation`, T ~ `Rep/Par hit`)) %>%
+  mutate(Annotation = ifelse(grepl("par", `PHROG/rep/par annotation`, ignore.case = TRUE), "DNA, RNA and nucleotide metabolism: Partition", `Protein category (PHROGs)`)) %>%
+  group_by(Annotation) %>% summarise(N = n()) %>% replace_na(list(Annotation = "not annotated")) %>% arrange(N)
+
+
+ggplot(Profile_information, aes(x = "", y = N, fill = reorder(Annotation, N))) +
+  geom_bar(stat = "identity", width = 0.5, color = "black") +
+  labs(
+    x = NULL, 
+    y = NULL,
+    fill = "Protein profile function categories"
+  ) +
+  theme_minimal(base_size = 15) +
+  theme(
+    legend.title = element_text(size = 14, face = "bold"),
+    legend.text = element_text(size = 14),
+    legend.position = "right",
+    axis.text.y = element_blank()
+  ) +
+  
+  scale_fill_manual(values = c("#6A9EFF", "#66D1C1", "#7FAAFF", "#FFB570", "#D3A8E6", 
+                               "#81D6A1", "#7AB8FF", "#FF9A9A", "#70C8FF", "#6DD9B3",  
+                               "#D4D4D4"))
+
+
+
+############### Figure S1: neff values by P-P type
+
+# NEFF is the diversity of the alignment, calculated as exp of the negative entropy averaged over all columns of the alignment
+
+Profile_information = read_excel("Study_associated_data/Supplementary_data/S2_Profile_information_table.xlsx")
+
+Profile_information %>% 
+  ggplot(aes(x = factor(`Neff value`))) +  
+  geom_bar(col = "darkgreen", fill = "darkgreen", width=0.7) +
+  theme_bw() + 
+  scale_y_continuous(breaks = pretty_breaks(n = 5)) +
+  scale_x_discrete(labels = c("1.0", "1.1", "1.2", "1.3", "1.4", "1.5")) + 
+  facet_wrap(~`P-P type`, scales = "free_y", nrow = 2) +
+  xlab("Neff value by P-P type") + 
+  ylab("Number of profiles") +
+  theme(strip.background = element_blank(), text = element_text(size = 17))
+
+
+
+############### Figure S2: Profile scores by P-P type 
+
+Profile_information = read_excel("Study_associated_data/Supplementary_data/S2_Profile_information_table.xlsx")
+
+ggplot(Profile_information, aes(x=x) ) +
+  geom_histogram( aes(x = `P-P score`), bins = 30 , fill="#378B85", col = "#0F595D" ) +
+  # geom_label( aes(x=0.3, y=150, label="P-P scores"), color="#378B85", size = 7) +
+  geom_histogram( aes(x = `P-P type score`,y=after_stat(-..count..)), bins = 30 , fill= "#88BB00", col = "#4C7500") +
+  # geom_label( aes(x=0.3, y=-150, label="P-P type scores"), color="#88BB00", size = 7) +
+  theme_bw() + 
+  facet_wrap(~`P-P type`, scales="free_y", nrow = 2) +
+  scale_y_continuous(breaks = pretty_breaks(n = 6))+
+  scale_x_continuous(breaks = seq(0,1,0.3))+
+  xlab(" Single profile specificity score values") + ylab("Number of profiles")+
+  theme(strip.background =element_blank(), text = element_text(size = 18))
+
+
+
+############### Figure S3: Functional annotation of signature profiles by P-P type (with PHROGs)
+
+Profile_information = read_excel("Study_associated_data/Supplementary_data/S2_Profile_information_table.xlsx")
+
+Profile_information_stat_by_type = Profile_information %>%
+  mutate(`PHROG/rep/par annotation` = case_when(!is.na(`PHROGs Annotation`) ~ `PHROGs Annotation`, T ~ `Rep/Par hit`)) %>%
+  mutate(Annotation = ifelse(grepl("par", `PHROG/rep/par annotation`, ignore.case = TRUE), "DNA, RNA and nucleotide metabolism: Partition", `Protein category (PHROGs)`)) %>%
+  group_by(Annotation, `P-P type`) %>% summarise(N = n()) %>% replace_na(list(Annotation = "not annotated")) %>% arrange(N)
+
+ggplot(Profile_information_stat_by_type, aes(x = "", y = N, fill = reorder(Annotation, N))) +
+  geom_bar(stat = "identity", width = 0.5, color = "black") +
+  labs(
+    x = NULL, 
+    y = NULL,
+    fill = "Protein profile function categories"
+  ) +
+  facet_wrap(~`P-P type`, nrow = 2) +
+  theme_minimal(base_size = 15) +
+  theme(
+    legend.title = element_text(size = 14, face = "bold"),
+    legend.text = element_text(size = 14),
+    legend.position = "right",
+    axis.text.y = element_blank()
+  ) +
+  
+  scale_fill_manual(values = c("#6A9EFF", "#66D1C1", "#7FAAFF", "#FFB570", "#D3A8E6", 
+                               "#81D6A1", "#7AB8FF", "#FF9A9A", "#70C8FF", "#6DD9B3",  
+                               "#D4D4D4"))
+
+
+########## Tables
+
+
+All_MGE_information_table = read_excel("Study_associated_data/Supplementary_data/S1_All_MGE_information_table.xlsx")
+
+Profile_information_table = read_excel("Study_associated_data/Supplementary_data/S2_Profile_information_table.xlsx")
+
